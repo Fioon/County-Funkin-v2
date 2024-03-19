9caefb91cc970fd750961d14ed1af462ba0fe47e
Fioon <135500399+Fioon@users.noreply.github.com>
2024-03-19 22:20:26 +0800

    Update MainMenuState.hx

--- source/meta/state/menus/MainMenuState.hx
+++ source/meta/state/menus/MainMenuState.hx
@@ -173,12 +173,12 @@ class MainMenuState extends MusicBeatState
 		move.y = FlxMath.lerp(optionGroup[selection].getMidpoint().y + 15, move.y, 1 - Main.framerateAdjust(0.15));
 		if (openedMenu)
 		{
-			if (controls.DOWN_P)
+			if (Control.UI_DOWN)
 			{
 				FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 				selection += 1;
 			}
-			if (controls.UP_P)
+			if (Control.UI_UP)
 			{
 				FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 				selection -= 1;
