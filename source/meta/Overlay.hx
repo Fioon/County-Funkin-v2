d12df000db1433376b50ed4d754c13fba4ad2f28
Fioon <135500399+Fioon@users.noreply.github.com>
2024-03-23 20:33:37 +0800

    Update Overlay.hx

--- source/meta/Overlay.hx
+++ source/meta/Overlay.hx
@@ -69,7 +69,7 @@ class Overlay extends TextField
 		{
 			text = '           ' // set up the text itself
 				+ (displayFps ? times.length + " FPS\n           " : '') // Framerate
-			#if !neko + (displayExtra ? Main.mainClassState + "\n            " : '') #end // Current Game State
+			#if !neko + (displayExtra ? Main.mainClassState + "\n           " : '') #end // Current Game State
 			+ (displayMemory ? '${getInterval(mem)} / ${getInterval(memPeak)}\n' : ''); // Current and Total Memory Usage
 		}
 	}
